b1ede37344c31e3602a3f788995edb1c0c9432bc
Jason Wu <jasonwu_8@hotmail.com>
2018-05-30 13:23:51 -0700

    Added some comments as a reminder. minor formatting.

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -6,6 +6,8 @@ import WorkExp from "../../data/WorkExperience";
 
 export default class Resume extends React.Component {
   render() {
+    /*cannot do this with `direction = "row"` because then it doesn't fill the width to maximun
+     cannot for the life of me figure out how to make this work with `xl = {6}` */
     return (
       <Grid
         container
@@ -21,11 +23,11 @@ export default class Resume extends React.Component {
             xs={12}
             sm={10}
             md={8}
-            xl={7}>
+            xl={7}
+          >
             <JobExperience job={job} />
           </Grid>
         )}
-
       </Grid>
     );
   }
