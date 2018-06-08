273f709d99b870f55f5b9a4ddcae88536d51bb8c
Jason Wu <jasonwu_8@hotmail.com>
2018-06-07 20:40:37 -0700

    Updates

--- src/components/resume/jobExperience/JobExperience.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import JobExperience from './JobExperience';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<JobExperience/>, div);
-});
