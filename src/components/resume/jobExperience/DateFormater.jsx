69b8ca67a3b8cee3871c3592cbcf340570ba4798
Jason Wu <h.jason.wu@gmail.com>
2018-06-08 19:12:12 -0700

    Changed the formatter from using 2016-01 to Jan 2016. (#19)

--- src/components/resume/jobExperience/DateFormater.jsx
+++ src/components/resume/jobExperience/DateFormater.jsx
@@ -1,13 +1,30 @@
 var moment = require("moment");
 
+const inputStringFormat = "YYYY-MM";
+
 export function formatStartEndDates(startDate, endDate) {
-  return (`${startDate} ${endDate ? endDate : "Current"} ${formatDuration(startDate, endDate)}`);
+  return `${formatDates(startDate, endDate)} ${formatDuration(startDate, endDate)}`
+}
+
+function formatDates(startDate, endDate) {
+  const startMoment = moment(startDate, inputStringFormat);
+  const endMoment = moment(endDate, inputStringFormat);
+
+  let startFormatString;
+  const endFormatString = "MMM YYYY";;
+
+  if (startMoment.year() === endMoment.year()) {
+    startFormatString = "MMM";
+  } else {
+    startFormatString = endFormatString;
+  }
+
+  return `${startMoment.format(startFormatString)} - ${endMoment.format(endFormatString)}`
 }
 
 function formatDuration(startDate, endDate) {
-  const formatString = "YYYY-MM";
-  const startMoment = moment(startDate, formatString);
-  const endMoment = endDate ? moment(endDate, formatString) : moment();
+  const startMoment = moment(startDate, inputStringFormat);
+  const endMoment = endDate ? moment(endDate, inputStringFormat) : moment();
   const durationMoment = moment.duration(endMoment.diff(startMoment));
 
   let duration = `${durationMoment.months()} Month${durationMoment.months() > 1 ? "s" : ""}`;
