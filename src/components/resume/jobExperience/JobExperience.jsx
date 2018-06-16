37ab7dced74a38f35d35e22e6e81baf438c2c782
Jason Wu <jasonwu_8@hotmail.com>
2018-06-15 17:18:08 -0700

    Massive overhaul of how this is laid out. added styling.

--- src/components/resume/jobExperience/JobExperience.jsx
+++ src/components/resume/jobExperience/JobExperience.jsx
@@ -1,75 +1,93 @@
 import React from "react";
 import { formatStartEndDates } from "./DateFormater";
-import "./JobExperience.css"
+import "./JobExperience.css";
 import Divider from '@material-ui/core/Divider';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import Chip from '@material-ui/core/Chip'
+import Chip from '@material-ui/core/Chip';
+import Grid from "@material-ui/core/Grid";
 
-const dividerChipStyle = {
+const dividerStyle = {
   margin: "1em",
 };
 
 const chipStyle = {
   margin: "0.25em",
-}
+};
 
 export default class JobExperience extends React.Component {
   render() {
     return (
       <Card className="card">
-        <CardContent className="content">
-          <Typography variant="headline" color="primary">
-            {this.props.job.company.name}
-          </Typography>
-          <Typography variant="title">
-            {this.props.job.title}
-          </Typography>
-          <Divider />
-          <Typography variant="caption" color="textSecondary" key={`${this.props.job}startEndDate`} gutterBottom>
-            {formatStartEndDates(this.props.job.startDate, this.props.job.endDate) + (this.props.job.contract ? " (Contract) " : "")}
-          </Typography>
-          {this.props.job.accomplishments.map(mapAccomplishments)}
-          <Divider style={dividerChipStyle} />
-          {this.props.job.technologies.sort().map(mapTechnologiesToChips)}
+        <CardContent className="content" component="div">
+          <Grid container spacing={0}>
+            <Grid item key="companyName" xs={12} sm={4}>
+              <Typography variant="headline" color="primary">
+                {this.props.job.company.name}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container spacing={0}>
+            <Grid item xs={6}>
+              <Typography variant="title">
+                {this.props.job.title}
+              </Typography>
+            </Grid>
+            <Grid item key="startEndDates" xs={6}>
+              <Typography variant="body1" color="textSecondary" key={`${this.props.job}startEndDate`} gutterBottom align="right">
+                {formatStartEndDates(this.props.job.startDate, this.props.job.endDate)} {(this.props.job.contract ? "(Contract)" : "")}
+              </Typography>
+            </Grid>
+            <Grid item key="startEndDateDivider" xs={12}>
+              <Divider style={dividerStyle} />
+            </Grid>
+            <Grid item key="accomplishmentList" xs={12}>
+              {formatAccomplishments(this.props.job.accomplishments)}
+            </Grid>
+            <Grid item key="accomplishmentsDivider" xs={12}>
+              <Divider style={dividerStyle} />
+            </Grid>
+            <Grid item key="technologyChips" xs={12}>
+              {this.props.job.technologies.sort().map(mapTechnologiesToChips)}
+            </Grid>
+          </Grid>
         </CardContent>
-      </Card>
+      </Card >
     );
   }
 }
 
-function mapAccomplishments(accomplishment, index) {
-  const sectionTitle = "title"
-  const accomplishmentVariant = "body2";
+function formatAccomplishments(accomplishments) {
+  const accomplishmentStyle = "body1";
+  let htmlContent = [];
 
-  let htmlContent = undefined;
-
-  if (accomplishment.hasOwnProperty("sectionTitle") && accomplishment.hasOwnProperty("sectionAccomplishments")) {
-    htmlContent = [
-      <Typography className="sectionTitle" variant={sectionTitle} key={accomplishment.sectionTitle}>
-        {accomplishment.sectionTitle}
-      </Typography>
-    ];
-    accomplishment.sectionAccomplishments.forEach(accomplishment => {
-      htmlContent.push(
-        <Typography className="sectionAccomplishments" variant={accomplishmentVariant} key={accomplishment}>
-          {accomplishment}
-        </Typography>)
-    });
+  if (accomplishments && typeof (accomplishments[0]) === 'string') {
+    htmlContent.push(
+      <ul>
+        <Typography variant={accomplishmentStyle} key={accomplishments}>
+          {accomplishments.map(x => <li key={x}>{x}</li>)}
+        </Typography>
+      </ul>
+    );
   } else {
-    htmlContent = (
-      <Typography variant={accomplishmentVariant} key={accomplishment}>
-        {accomplishment}
-      </Typography>
+    accomplishments.forEach(x => {
+      htmlContent.push(
+        <Typography variant={accomplishmentStyle} key={x.sectionTitle}>
+          {x.sectionTitle}
+        </Typography>
+      );
+      htmlContent.push(
+        <ul key={x.sectionAccomplishments}>
+          <Typography variant={accomplishmentStyle} key={x.sectionAccomplishments}>
+            {x.sectionAccomplishments.map(section => <li key={section}>{section}</li>)}
+          </Typography>
+        </ul>
+      );
+    }
     );
   }
 
-  if (htmlContent === undefined) {
-    const errorMessage = `unhandled accomplishment encountered at ${index}. ${accomplishment}`
-    console.log(errorMessage);
-    throw errorMessage;
-  }
   return htmlContent;
 }
 
@@ -79,4 +97,19 @@ function mapTechnologiesToChips(technology, index) {
     key={technology}
     style={chipStyle}
     className="technologyChip" />
+}
+
+JobExperience.defaultProps = {
+  job: {
+    company: {
+      name: "default"
+    },
+    accomplishments: [
+      {
+        sectionTitle: "defaultSection",
+        sectionAccomplishments: []
+      }
+    ],
+    technologies: [],
+  }
 }
