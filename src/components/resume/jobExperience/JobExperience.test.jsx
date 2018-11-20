6c530f75ae74c21ee1494f35e7fba99c53c7ca6e
Jason Wu <jasonwu_8@hotmail.com>
2018-11-19 19:55:59 -0800

    Changed test to be shallow

--- src/components/resume/jobExperience/JobExperience.test.jsx
+++ src/components/resume/jobExperience/JobExperience.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import JobExperience from './JobExperience';
 
+import { shallow } from 'enzyme';
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<JobExperience/>, div);
+  shallow(<JobExperience />);
 });
