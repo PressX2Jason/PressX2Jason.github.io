0fdb25077fe543a97e1b99b8caa333b2a8937762
Jason Wu <jasonwu_8@hotmail.com>
2018-12-19 14:47:07 -0800

    Added front facing photo. WIP

--- src/components/resume/aboutMe/AboutMe.jsx
+++ src/components/resume/aboutMe/AboutMe.jsx
@@ -3,12 +3,14 @@ import "./AboutMe.css"
 
 import StyledButton from './styledButton/StyledButton';
 
+import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
+import Hidden from '@material-ui/core/Hidden';
 
 import LinkedinBox from 'mdi-material-ui/Linkedin';
 import GithubBox from 'mdi-material-ui/GithubCircle';
@@ -17,7 +19,7 @@ import Phone from '@material-ui/icons/Phone';
 import Email from '@material-ui/icons/Email';
 
 
-const avatar1961w = require("../../../data/images/avatar-1961w.jpg");
+const avatar = require("../../../data/images/avatar.jpg");
 
 const card = {
     display: "flex",
@@ -31,6 +33,12 @@ const avatarStyle = {
     margin: "auto",
 };
 
+const circleAvatarStyle = {
+    height : '3em',
+    width: '3em',
+    justify : 'center',
+};
+
 const dividerStyle = {
     margin: "1em",
 };
@@ -40,8 +48,8 @@ export default class AboutMe extends React.Component {
         return (
             <Card style={card}>
                 <Grid container spacing={16}>
-                    <Grid item xs={12} sm={4}>
-                        <CardMedia image={avatar1961w} style={avatarStyle} title="That's me!" />
+                    <Grid item xs={0} sm={4}>
+                        <CardMedia image={avatar} style={avatarStyle} title="That's me on the beach of Nice, France" />
                     </Grid>
                     <Grid item xs={12} sm={8}>
                         <CardContent>
@@ -53,7 +61,19 @@ export default class AboutMe extends React.Component {
                                 spacing={16}
                             >
                                 <Grid item xs={12}>
-                                    <Typography variant="headline">{this.props.name}</Typography>
+                                    <Grid
+                                        spacing={0}
+                                        container
+                                     >
+                                        <Hidden smUp>
+                                            <Grid item xs={4}>
+                                                <Avatar style={circleAvatarStyle} src={avatar}/>
+                                            </Grid>
+                                        </Hidden>
+                                        <Grid item xs={8} sm={16}>
+                                            <Typography variant="h3">{this.props.name}</Typography>
+                                        </Grid>
+                                    </Grid>
                                 </Grid>
                                 <Grid item xs={12}>
                                     <Divider light style={dividerStyle} />
@@ -72,16 +92,16 @@ export default class AboutMe extends React.Component {
                                     container
                                     spacing={16}
                                 >
-                                    <Grid item xs={12} sm={6} md={3} >
+                                    <Grid item xs={3} sm={6} md={3} >
                                         <StyledButton icon={<LinkedinBox titleAccess="Linkedin Icon" />} href={this.props.contact.linkedin} />
                                     </Grid>
-                                    <Grid item xs={12} sm={6} md={3} >
+                                    <Grid item xs={3} sm={6} md={3} >
                                         <StyledButton icon={<GithubBox titleAccess="Github Icon" />} href={this.props.contact.github} />
                                     </Grid>
-                                    <Grid item xs={12} sm={6} md={3} >
+                                    <Grid item xs={3} sm={6} md={3} >
                                         <StyledButton icon={<Phone titleAccess="Phone number" />} href={`tel:+1-${this.props.contact.phone}`} />
                                     </Grid>
-                                    <Grid item xs={12} sm={6} md={3} >
+                                    <Grid item xs={3} sm={6} md={3} >
                                         <StyledButton icon={<Email titleAccess="GMail" />} href={`mailto:${this.props.contact.email}`} />
                                     </Grid>
                                     <Grid item xs={12} >
