2b1bd26fa99baf827a8bed97cb55b97e5484ff6e
Jason Wu <jasonwu_8@hotmail.com>
2019-04-01 02:54:13 -0700

    Moved function into render

--- src/components/resume/accomplishment/Accomplishment.jsx
+++ src/components/resume/accomplishment/Accomplishment.jsx
@@ -5,47 +5,41 @@ import Typography from '@material-ui/core/Typography';
 
 export default class Accomplishment extends React.Component {
     render() {
-        return (
-            formatAccomplishments(this.props.accomplishment)
-        );
-    }
-}
-
-function formatAccomplishments(accomplishment) {
-    const textVariant = 'body1';
-    let htmlContent = [];
-
-    if (accomplishment && typeof (accomplishment[0]) === 'string') {
-        htmlContent.push(
-            <ul key={accomplishment}>
-                <Typography variant={textVariant} key={accomplishment}>
-                    {accomplishment.map(x => <li key={x}>{x}</li>)}
-                </Typography>
-            </ul>
-        );
-    } else {
-        accomplishment.forEach(x => {
+        const textVariant = 'body1';
+        let htmlContent = [];
+    
+        if (this.props.accomplishment && typeof (this.props.accomplishment[0]) === 'string') {
             htmlContent.push(
-                <Typography variant={textVariant} key={x.sectionTitle}>
-                    {x.sectionTitle}
-                </Typography>
-            );
-            htmlContent.push(
-                <ul key={x.sectionAccomplishments}>
-                    <Typography variant={textVariant} key={x.sectionAccomplishments}>
-                        {x.sectionAccomplishments.map(section => <li key={section}>{section}</li>)}
+                <ul key={this.props.accomplishment}>
+                    <Typography variant={textVariant} key={this.props.accomplishment}>
+                        {this.props.accomplishment.map(x => <li key={x}>{x}</li>)}
                     </Typography>
                 </ul>
             );
-        }
-        );
-    }
+        } else {
+            this.props.accomplishment.forEach(x => {
+                htmlContent.push(
+                    <Typography variant={textVariant} key={x.sectionTitle}>
+                        {x.sectionTitle}
+                    </Typography>
+                );
+                htmlContent.push(
+                    <ul key={x.sectionAccomplishments}>
+                        <Typography variant={textVariant} key={x.sectionAccomplishments}>
+                            {x.sectionAccomplishments.map(section => <li key={section}>{section}</li>)}
+                        </Typography>
+                    </ul>
+                );
+            }
+            );
+        }    
 
-    return htmlContent;
+        return (htmlContent);
+    }
 }
 
 Accomplishment.defaultProps = {
-    accomplishments: [
+    accomplishment: [
         {
             sectionTitle: 'default',
             sectionAccomplishments: []
